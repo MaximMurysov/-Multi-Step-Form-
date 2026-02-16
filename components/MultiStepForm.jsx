ab2a2febe9a314153b8d4a6e8c5e6acebda8e52f
Maxim Murysov <murisov98@mail.ru>
2026-02-16 09:41:45 +0300

    first commit

---
+++ components/MultiStepForm.jsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.css";
+import { useState } from "react";
+import { steps } from "./main";
+
+function MultiStepForm() {
+  return <div></div>;
+}
+export default MultiStepForm;
