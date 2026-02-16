6468fa108f2409f273b17f72271b0c4a0b0303ee
Maxim Murysov <murisov98@mail.ru>
2026-02-16 10:26:15 +0300

    feat: add form layout with step indicator and fields

--- src/components/MultiStepForm.jsx
+++ src/components/MultiStepForm.jsx
@@ -3,6 +3,28 @@ import { useState } from "react";
 import { steps } from "./main";
 
 function MultiStepForm() {
-  return <div></div>;
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className={styles.mutliStep}>
+      <div className={styles.mutliStepContainer}>
+        <div className={styles.mutliStepForm}>
+          <div className={styles.mutliStepFormPages}>
+            {steps.map((_, i) => (
+              <h1 className={styles.page}>{i + 1}</h1>
+            ))}
+          </div>
+          <h2>{steps[index].heading}</h2>
+          {steps[index].fields.map((elem, i) => (
+            <div key={i}>
+              <p>{elem}</p>
+              <input type="text" />
+            </div>
+          ))}
+          <button>Next</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default MultiStepForm;
