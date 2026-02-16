bd01fe933d5f02d7333193a9c47178dc0b7a9e1a
Maxim Murysov <murisov98@mail.ru>
2026-02-16 09:46:44 +0300

    Moved components to the src folder

---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+export const steps = [
+  {
+    heading: "Personal Info",
+    fields: ["name", "age"],
+  },
+  {
+    heading: "Location Info",
+    fields: ["city", "street", "house"],
+  },
+  {
+    heading: "Professional Info",
+    fields: ["company", "grade"],
+  },
+  {
+    heading: "Signup info",
+    fields: ["email", "phone"],
+  },
+];
